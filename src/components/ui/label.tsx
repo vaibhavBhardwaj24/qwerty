49bd0f47c6702bfd43c483ca2b02de1ae62bec14
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2024-08-15 17:14:11 +0530

    :)

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import { cn } from "../../utils/cn.ts";
+import { cn } from "../../utils/cn";
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
